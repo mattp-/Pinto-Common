6474dc07cc5d404c9c1e02da0446390ee66e98e6
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2012-04-16 16:25:54 -0700

    Added PackageSpec and DistributionSpec classes.
    These provide some notation for specifying packages and distributions as strings.

---
+++ lib/Pinto/PackageSpec.pm
@@ -0,0 +1,62 @@
+# ABSTRACT: Specifies a package by name and version
+
+package Pinto::PackageSpec;
+
+use Moose;
+
+use MooseX::Types::Moose qw(Str);
+use Pinto::Types qw(Vers);
+
+use overload ('""' => 'to_string');
+
+#------------------------------------------------------------------------------
+
+# VERSION
+
+#------------------------------------------------------------------------------
+
+has name => (
+    is       => 'ro',
+    isa      => Str,
+    required => 1,
+);
+
+
+has version => (
+    is      => 'ro',
+    isa     => Vers,
+    coerce  => 1,
+    default => sub { version->parse(0) }
+);
+
+#------------------------------------------------------------------------------
+
+around BUILDARGS => sub {
+    my $orig = shift;
+    my $class = shift;
+
+    my @args = @_;
+    if (@args == 1 and not ref $args[0]) {
+        my ($name, $version) = split m{-}, $_[0], 2;
+        @args = (name => $name, version => $version || 0);
+    }
+
+    return $class->$orig(@args);
+};
+
+#------------------------------------------------------------------------------
+
+sub to_string {
+    my ($self) = @_;
+    return sprintf '%s-%s', $self->name, $self->version->stringify;
+}
+
+#------------------------------------------------------------------------------
+
+__PACKAGE__->meta->make_immutable;
+
+#------------------------------------------------------------------------------
+1;
+
+__END__
+
