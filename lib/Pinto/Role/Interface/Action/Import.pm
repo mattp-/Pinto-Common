1fc245b5fb3900e5badf1b263b9de317f121b2ba
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2012-04-16 16:31:34 -0700

    Change Import interface to use a "target" attribute.
    This attribute can be either a PackageSpec or a DistributionSpec.

--- lib/Pinto/Role/Interface/Action/Import.pm
+++ lib/Pinto/Role/Interface/Action/Import.pm
@@ -4,6 +4,9 @@ package Pinto::Role::Interface::Action::Import;
 
 use Moose::Role;
 
+use Pinto::PackageSpec;
+use Pinto::DistributionSpec;
+
 use namespace::autoclean;
 
 #------------------------------------------------------------------------------
@@ -13,9 +16,36 @@ use namespace::autoclean;
 #------------------------------------------------------------------------------
 
 with qw( Pinto::Role::Interface::Action
-         Pinto::Role::Attribute::package
-         Pinto::Role::Attribute::version
          Pinto::Role::Attribute::norecurse );
+
+#------------------------------------------------------------------------------
+
+has target => (
+    is       => 'ro',
+    isa      => 'Pinto::PackageSpec | Pinto::DistributionSpec',
+    required => 1,
+);
+
+#------------------------------------------------------------------------------
+
+around BUILDARGS => sub {
+    my $orig = shift;
+    my $class = shift;
+    my %args = @_;
+
+    # If the target is not a reference, then we presume it is a
+    # specification string.  If the string contains a slash then it
+    # must be a DistributionSpec.  Otherwise it must be a PackageSpec.
+
+    if ($args{target} and not ref $args{target}) {
+        my $target = delete $args{target};
+        $args{target} = $target =~ m{/}x ? Pinto::DistributionSpec->new($target)
+                                         : Pinto::PackageSpec->new($target);
+    }
+
+    return $class->$orig(%args);
+};
+
 
 #------------------------------------------------------------------------------
 
