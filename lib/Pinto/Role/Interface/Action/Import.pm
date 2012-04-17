b8fdc2487cb2fcec373ccbaaf978ba4db0826ad0
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2012-04-16 17:40:39 -0700

    Move logic for the target out into an attribute role.

--- lib/Pinto/Role/Interface/Action/Import.pm
+++ lib/Pinto/Role/Interface/Action/Import.pm
@@ -4,9 +4,6 @@ package Pinto::Role::Interface::Action::Import;
 
 use Moose::Role;
 
-use Pinto::PackageSpec;
-use Pinto::DistributionSpec;
-
 use namespace::autoclean;
 
 #------------------------------------------------------------------------------
@@ -16,36 +13,8 @@ use namespace::autoclean;
 #------------------------------------------------------------------------------
 
 with qw( Pinto::Role::Interface::Action
+         Pinto::Role::Attribute::target
          Pinto::Role::Attribute::norecurse );
-
-#------------------------------------------------------------------------------
-
-has target => (
-    is       => 'ro',
-    isa      => 'Pinto::PackageSpec | Pinto::DistributionSpec',
-    required => 1,
-);
-
-#------------------------------------------------------------------------------
-
-around BUILDARGS => sub {
-    my $orig = shift;
-    my $class = shift;
-    my %args = @_;
-
-    # If the target is not a reference, then we presume it is a
-    # specification string.  If the string contains a slash then it
-    # must be a DistributionSpec.  Otherwise it must be a PackageSpec.
-
-    if ($args{target} and not ref $args{target}) {
-        my $target = delete $args{target};
-        $args{target} = $target =~ m{/}x ? Pinto::DistributionSpec->new($target)
-                                         : Pinto::PackageSpec->new($target);
-    }
-
-    return $class->$orig(%args);
-};
-
 
 #------------------------------------------------------------------------------
 
