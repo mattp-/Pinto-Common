8b6f151a325966ab53e1d2976e8d6241f68b8570
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2012-04-24 21:04:55 -0700

    Type coercions for the Archives type.

--- lib/Pinto/Role/Attribute/archives.pm
+++ lib/Pinto/Role/Attribute/archives.pm
@@ -3,8 +3,12 @@
 package Pinto::Role::Attribute::archives;
 
 use Moose::Role;
+use Moose::Util::TypeConstraints;
 use MooseX::Types::Moose qw(ArrayRef);
-use Pinto::Types qw( File );
+
+use Path::Class qw(file);
+use Scalar::Util qw(blessed);
+use List::MoreUtils qw(all);
 
 use namespace::autoclean;
 
@@ -13,10 +17,32 @@ use namespace::autoclean;
 # VERSION
 
 #------------------------------------------------------------------------------
+# TODO: Come up with a custom Type and coercion to do the right thing with...
+# ArrayRef of objects, ArrayRef of strings, single object, single string
+
+subtype 'Archives',
+    as  'ArrayRef',
+    where { all { blessed $_ and $_->isa('Path::Class::File') } @$_ },
+              message { 'Not an array of Path::Class::File objects' };
+
+coerce   'Archives',
+    from 'ArrayRef',
+    via  { [ map {file("$_")} @$_ ] };
+
+coerce   'Archives',
+    from 'Str',
+    via  { [ file("$_") ] };
+
+class_type 'Path::Class::File';
+
+coerce   'Archives',
+    from 'Path::Class::File',
+    via  { [ $_ ] };
+
+#------------------------------------------------------------------------------
 
 has archives  => (
-    is       => 'ro',
-    isa      => ArrayRef[File],
+    isa      => 'Archives',
     coerce   => 1,
     required => 1,
     traits   => [ qw(Array) ],
--- lib/Pinto/Role/Attribute/targets.pm
+++ lib/Pinto/Role/Attribute/targets.pm
@@ -1,8 +1,9 @@
 # ABSTRACT: Something that has a target attribute
 
-package Pinto::Role::Attribute::target;
+package Pinto::Role::Attribute::targets;
 
 use Moose::Role;
+use Moose::Util::TypeConstraints;
 
 use Pinto::PackageSpec;
 use Pinto::DistributionSpec;
@@ -14,36 +15,39 @@ use namespace::autoclean;
 # VERSION
 
 #------------------------------------------------------------------------------
+# TODO: Come up with a custom Type and coercion to do the right thing with...
+# ArrayRef of objects, ArrayRef of strings, single object, single string
 
-with qw( Pinto::Meta::Attribute::Trait::Postable );
-
-#------------------------------------------------------------------------------
-
-has target => (
-    is       => 'ro',
-    isa      => 'Pinto::PackageSpec | Pinto::DistributionSpec',
-    traits   => [ qw(Postable) ],
-    post_via => sub { $_[0]->target->to_string },
+has targets => (
+    isa      => 'ArrayRef',
+    traits   => [ qw(Array) ],
+    handles  => {targets => 'elements'},
     required => 1,
 );
 
 #------------------------------------------------------------------------------
 
 around BUILDARGS => sub {
-    my $orig = shift;
-    my $class = shift;
-    my %args = @_;
+    my ($orig, $class, %args) = @_;
 
-    # If the target is not a reference, then we presume it is a
-    # specification string.  If the string contains a slash then it
-    # must be a DistributionSpec.  Otherwise it must be a PackageSpec.
+    # Convert a scalar to a one element array
+    my $targets = delete $args{targets};
+    $targets = [ $targets || () ] if ref $targets ne 'ARRAY';
 
-    if ($args{target} and not ref $args{target}) {
-        my $target = delete $args{target};
-        $args{target} = $target =~ m{/}x ? Pinto::DistributionSpec->new($target)
-                                         : Pinto::PackageSpec->new($target);
+    my @objects;
+    for my $target ( @{ $targets } ){
+
+        # If the target is not a reference, then we presume it is a
+        # specification string.  If the string contains a slash then
+        # it must be a DistributionSpec.  Otherwise it must be a
+        # PackageSpec.
+
+        push @objects, ref $target ? $target :
+                ($target =~ m{/}x) ? Pinto::DistributionSpec->new($target)
+                                   : Pinto::PackageSpec->new($target);
     }
 
+    $args{targets} = \@objects;
     return $class->$orig(%args);
 };
 
