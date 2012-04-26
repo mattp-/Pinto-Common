870620d1b9a01bb884107fe7b634524bba35db88
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2012-04-25 20:22:09 -0700

    Remove inflator for stack attribute.
    This is only relevant to Pinto, not Pinto-Remote.

--- lib/Pinto/Role/Attribute/stack.pm
+++ lib/Pinto/Role/Attribute/stack.pm
@@ -4,7 +4,7 @@ package Pinto::Role::Attribute::stack;
 
 use Moose::Role;
 
-use Carp;
+use Pinto::Types qw(StackName);
 
 use namespace::autoclean;
 
@@ -14,28 +14,12 @@ use namespace::autoclean;
 
 #------------------------------------------------------------------------------
 
-requires qw(repos);
-
-#------------------------------------------------------------------------------
-
 has stack => (
     is       => 'ro',
-    isa      => 'Pinto::Schema::Result::Stack',
-    writer   => '_set_stack',
-    init_arg => undef,
+    isa      => StackName,
+    default  => 'default',
+    coerce   => 1,
 );
-
-#------------------------------------------------------------------------------
-
-sub BUILD {};
-
-before BUILD => sub {
-    my ($self, $args) = @_;
-    my $stack_name = $args->{stack} || 'default';
-    my $stack = $self->repos->get_stack(name => $stack_name);
-    confess "No such stack $stack_name" if not $stack;
-    $self->_set_stack($stack);
-};
 
 #------------------------------------------------------------------------------
 
