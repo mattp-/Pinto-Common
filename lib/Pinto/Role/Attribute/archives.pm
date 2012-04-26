167c198e090c7c89c67195fd3b416b257e4da9a7
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2012-04-26 15:22:42 -0700

    Use new ArrayRefOfFiles type.

--- lib/Pinto/Role/Attribute/archives.pm
+++ lib/Pinto/Role/Attribute/archives.pm
@@ -1,14 +1,10 @@
-# ABSTRACT: Something that has a distribution archive attribute
+# ABSTRACT: Something that has a distribution archives attribute
 
 package Pinto::Role::Attribute::archives;
 
 use Moose::Role;
-use Moose::Util::TypeConstraints;
-use MooseX::Types::Moose qw(ArrayRef);
 
-use Path::Class qw(file);
-use Scalar::Util qw(blessed);
-use List::MoreUtils qw(all);
+use Pinto::Types qw(ArrayRefOfFiles);
 
 use namespace::autoclean;
 
@@ -17,36 +13,13 @@ use namespace::autoclean;
 # VERSION
 
 #------------------------------------------------------------------------------
-# TODO: Come up with a custom Type and coercion to do the right thing with...
-# ArrayRef of objects, ArrayRef of strings, single object, single string
-
-subtype 'Archives',
-    as  'ArrayRef',
-    where { all { blessed $_ and $_->isa('Path::Class::File') } @$_ },
-              message { 'Not an array of Path::Class::File objects' };
-
-coerce   'Archives',
-    from 'ArrayRef',
-    via  { [ map {file("$_")} @$_ ] };
-
-coerce   'Archives',
-    from 'Str',
-    via  { [ file("$_") ] };
-
-class_type 'Path::Class::File';
-
-coerce   'Archives',
-    from 'Path::Class::File',
-    via  { [ $_ ] };
-
-#------------------------------------------------------------------------------
 
 has archives  => (
-    isa      => 'Archives',
-    coerce   => 1,
-    required => 1,
-    traits   => [ qw(Array) ],
-    handles  => {archives => 'elements'}
+    isa       => ArrayRefOfFiles,
+    traits    => [ qw(Array) ],
+    handles   => {archives => 'elements'},
+    required  => 1,
+    coerce    => 1,
 );
 
 #------------------------------------------------------------------------------
